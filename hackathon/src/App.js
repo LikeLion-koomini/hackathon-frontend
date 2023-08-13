cad74b5a8f06b1975d50b026375a090e197ccf9b
Hongki <yunszone98@naver.com>
2023-08-14 02:42:35 +0900

    MainPage UI

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage/LoginPage";
+import MainPage from "./components/MainPage/MainPage";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
--- hackathon/src/assets/images/images.js
+++ hackathon/src/assets/images/images.js
@@ -1,2 +1,12 @@
-const images = {};
+const images = {
+  mainPageImg1: require('../images/mainPageImg1.png'),
+  mainPageImg2: require('../images/mainPageImg2.png'),
+  mainPageImg3: require('../images/mainPageImg3.png'),
+  mainPageImg4: require('../images/mainPageImg4.png'),
+  mainPageImg5: require('../images/mainPageImg5.png'),
+  mainPageImg6: require('../images/mainPageImg6.png'),
+  mainPageImg7: require('../images/mainPageImg7.png'),
+  mainPageImg8: require('../images/mainPageImg8.png'),
+  mainPageImg9: require('../images/mainPageImg9.png'),
+};
 export default images;
