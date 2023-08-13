40d039a07130eee74713c72c86a506d2d63adad0
kym8821 <louie9798@naver.com>
2023-08-14 02:03:10 +0900

    first commit

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyPage from "./components/MyPage";
+import ColumnCreatePage from './components/ColumnCreatePage/ColumnCreatePage';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/column/create" element={<ColumnCreatePage />} />
         </Routes>
       </Router>
     </div>
