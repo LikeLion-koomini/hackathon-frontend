4f2102a8f7b972bdea8a0efaab2437c0205b1a2e
kym8821 <louie9798@naver.com>
2023-08-14 02:27:08 +0900

    columnDetailPage

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ColumnCreatePage from './components/ColumnCreatePage/ColumnCreatePage';
+import ColumnDetailPage from './components/ColumnDetailPage/ColumnDetailPage';
 import LoginPage from "./components/LoginPage/LoginPage";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/column/create" element={<ColumnCreatePage />} />
+          <Route path="/column/detail" element={<ColumnDetailPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
