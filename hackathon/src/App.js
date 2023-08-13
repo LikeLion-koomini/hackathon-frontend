99b48886d1364296ab51eedf232e942cedbe047f
kym8821 <127489230+kym8821@users.noreply.github.com>
2023-08-14 02:11:04 +0900

    first commit (#2)
    merge success

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ColumnCreatePage from './components/ColumnCreatePage/ColumnCreatePage';
 import LoginPage from "./components/LoginPage/LoginPage";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/column/create" element={<ColumnCreatePage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
---
+++ hackathon/src/components/ColumnCreatePage/ColumnCreatePage.js
@@ -0,0 +1,137 @@
+import React, {useRef, useState, useEffect, useMemo} from 'react';
+import styles from "./ColumnCreatePage.module.css"
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const categoryNameList = [
+  "tag1",
+  "tag2",
+  "tag3",
+  "long long tag",
+  "very long tag",
+  "so long tag",
+]
+
+const ColumnCreatePage = ()=>{
+  const [categoryJsxList, setCategoryJsxList] = useState([]);
+  const [content, setContent] = useState();
+  const quillRef = useRef()
+
+  // 기본 설정
+  useEffect(()=>{
+    setCategoryJsxList((prev)=>{
+      return [<button className={styles.categoryManager}>카테고리 추가 / 삭제</button>]
+    })
+    let key = 0;
+    categoryNameList.forEach((item)=>{
+      setCategoryJsxList((prev)=>{
+        return [...prev,<div className={styles.category} key={key++}>{item}</div>]
+      })
+    })
+  }, []);
+
+  const imageHandler = () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.addEventListener('change', async () => {
+      if(input.files){
+        const file = input.files[0];
+        try {
+          const fileReader = new FileReader()
+          fileReader.onload = (e) => {
+            const imgUrl = fileReader.result;
+            console.log(typeof(imgUrl))
+            const editor = quillRef.current.getEditor(); 
+            const range = editor.getSelection();
+            editor.insertEmbed(range.index, 'image', imgUrl);
+            editor.setSelection(range.index + 1);
+          }
+          fileReader.readAsDataURL(file);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  };
+  
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: '1' }, { header: '2' }],
+          [{ size: [] }],
+          ['bold', 'italic', 'underline'],
+          [{ list: 'ordered' }, { list: 'bullet' }, { align: [] }],
+          ['image'],
+        ],
+        handlers: { image: imageHandler },
+      },
+      clipboard: {
+        matchVisual: false,
+      },
+    }),
+    [],
+  );
+
+  const handleSubmitButton = (event)=>{
+    console.log(content)
+  }
+  const handleQuillChange = (elements)=>{
+    setContent(()=>elements)
+  }
+  return(
+    <div className={styles.background}>
+      <input type="text" className={styles.columnName} placeholder='칼럼 제목 입력하기'/>
+      <div className={styles.contentInputBox}>
+        <span className={styles.inputBoxTitle}>
+          칼럼 내용
+        </span>
+        <div>
+          <ReactQuill 
+            onChange={handleQuillChange}
+            modules={modules} 
+            ref={quillRef}
+            theme="snow"
+          />
+        </div>
+      </div>
+      <div className={styles.contentInputBox}>
+        <span className={styles.inputBoxTitle}>
+          카테고리
+        </span>
+        <div className={styles.categoryList}>
+          {categoryJsxList}
+        </div>
+      </div>
+      <div className={styles.contentInputBox}>
+        <span className={styles.inputBoxTitle}>
+          가격
+        </span>
+        <div className={styles.priceCheckBoxContainer}>
+          <div className={styles.priceCheckBox}>
+            <div className={styles.checkBox}>
+              <input type="radio" id="free" name="price" value="free" />
+              <label htmlFor='free'>무료</label>
+            </div>
+          </div>
+          <div className={styles.priceCheckBox}>
+            <div className={styles.checkBox}>
+              <input type="radio" name="price" id="free" value="pay" />
+              <label htmlFor="pay">유료</label>
+            </div>
+            <div className={styles.priceBox}>
+              <label htmlFor="price">가격</label>
+              <input type="text"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className={styles.submitButton} onClick={handleSubmitButton}>발행하기</button>
+    </div>
+  )
+}
+
+export default ColumnCreatePage;
