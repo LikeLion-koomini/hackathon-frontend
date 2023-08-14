23c52f102e1a283b066631645e287a32b10f85ef
kym8821 <127489230+kym8821@users.noreply.github.com>
2023-08-14 16:10:51 +0900

    modal and topbar (#5)
    * 카테고리 창과 모달창

--- hackathon/src/components/ColumnCreatePage/ColumnCreatePage.js
+++ hackathon/src/components/ColumnCreatePage/ColumnCreatePage.js
@@ -1,5 +1,7 @@
 import React, {useRef, useState, useEffect, useMemo} from 'react';
 import styles from "./ColumnCreatePage.module.css"
+import CategoryModal from "../CategoryModal/CategoryModal"
+import Topbar from '../Topbar/Topbar';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -15,17 +17,29 @@ const categoryNameList = [
 const ColumnCreatePage = ()=>{
   const [categoryJsxList, setCategoryJsxList] = useState([]);
   const [content, setContent] = useState();
+  const [categoryModalActive, setCategoryModalActive] = useState(false)
   const quillRef = useRef()
 
   // 기본 설정
+  const categoryModalHandler = ()=>{
+    setCategoryModalActive((prev)=>!prev)
+  }
   useEffect(()=>{
     setCategoryJsxList((prev)=>{
-      return [<button className={styles.categoryManager}>카테고리 추가 / 삭제</button>]
+      return [
+        <button
+          className={styles.categoryManager}
+          key={0}
+          onClick={categoryModalHandler}
+        >
+          카테고리 추가 / 삭제
+        </button>
+      ]
     })
     let key = 0;
     categoryNameList.forEach((item)=>{
       setCategoryJsxList((prev)=>{
-        return [...prev,<div className={styles.category} key={key++}>{item}</div>]
+        return [...prev,<div className={styles.category} key={++key}>{item}</div>]
       })
     })
   }, []);
@@ -83,53 +97,59 @@ const ColumnCreatePage = ()=>{
     setContent(()=>elements)
   }
   return(
-    <div className={styles.background}>
-      <input type="text" className={styles.columnName} placeholder='칼럼 제목 입력하기'/>
-      <div className={styles.contentInputBox}>
-        <span className={styles.inputBoxTitle}>
-          칼럼 내용
-        </span>
-        <div>
-          <ReactQuill 
-            onChange={handleQuillChange}
-            modules={modules} 
-            ref={quillRef}
-            theme="snow"
-          />
-        </div>
-      </div>
-      <div className={styles.contentInputBox}>
-        <span className={styles.inputBoxTitle}>
-          카테고리
-        </span>
-        <div className={styles.categoryList}>
-          {categoryJsxList}
-        </div>
-      </div>
-      <div className={styles.contentInputBox}>
-        <span className={styles.inputBoxTitle}>
-          가격
-        </span>
-        <div className={styles.priceCheckBoxContainer}>
-          <div className={styles.priceCheckBox}>
-            <div className={styles.checkBox}>
-              <input type="radio" id="free" name="price" value="free" />
-              <label htmlFor='free'>무료</label>
-            </div>
-          </div>
-          <div className={styles.priceCheckBox}>
-            <div className={styles.checkBox}>
-              <input type="radio" name="price" id="free" value="pay" />
-              <label htmlFor="pay">유료</label>
-            </div>
-            <div className={styles.priceBox}>
-              <label htmlFor="price">가격</label>
-              <input type="text"/>
-            </div>
+    <div style={{display:"flex", flexDirection:"column",alignItems:"center",}}>
+      <Topbar current="normal" isLogin={false}/>
+      {categoryModalActive && 
+        <CategoryModal setModalActive={setCategoryModalActive}/>
+      }
+      <div className={styles.background}>
+        <input type="text" className={styles.columnName} placeholder='칼럼 제목 입력하기'/>
+        <div className={styles.contentInputBox}>
+          <span className={styles.inputBoxTitle}>
+            칼럼 내용
+          </span>
+          <div>
+            <ReactQuill 
+              onChange={handleQuillChange}
+              modules={modules} 
+              ref={quillRef}
+              theme="snow"
+            />
           </div>
         </div>
+        <div className={styles.contentInputBox}>
+          <span className={styles.inputBoxTitle}>
+            카테고리
+          </span>
+          <div className={styles.categoryList}>
+            {categoryJsxList}
+          </div>
+        </div>
+        <div className={styles.contentInputBox}>
+          <span className={styles.inputBoxTitle}>
+            가격
+          </span>
+          <div className={styles.priceCheckBoxContainer}>
+            <div className={styles.priceCheckBox}>
+              <div className={styles.checkBox}>
+                <input type="radio" id="free" name="price" value="free" />
+                <label htmlFor='free'>무료</label>
+              </div>
+            </div>
+            <div className={styles.priceCheckBox}>
+              <div className={styles.checkBox}>
+                <input type="radio" name="price" id="free" value="pay" />
+                <label htmlFor="pay">유료</label>
+              </div>
+              <div className={styles.priceBox}>
+                <label htmlFor="price">가격</label>
+                <input type="text"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button className={styles.submitButton} onClick={handleSubmitButton}>발행하기</button>
       </div>
-      <button className={styles.submitButton} onClick={handleSubmitButton}>발행하기</button>
     </div>
   )
 }
