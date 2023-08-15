53252c46a6f3f7169dd7625d7ad16a715c738a12
Yun-hongki <84968311+Yun-hongki@users.noreply.github.com>
2023-08-15 23:12:05 +0900

    Series page (#4)
    * MainPage UI
    
    * SeriesPage UI
    
    * modal and topbar (#5)
    
    * 카테고리 창과 모달창
    
    ---------
    
    Co-authored-by: kym8821 <127489230+kym8821@users.noreply.github.com>
    Co-authored-by: kym8821 <louie9798@naver.com>

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ColumnCreatePage from './components/ColumnCreatePage/ColumnCreatePage';
 import LoginPage from "./components/LoginPage/LoginPage";
+import MainPage from "./components/MainPage/MainPage";
+import SeriesList from "./components/SeriesPage/SeriesList";
+import SeriesCreate from "./components/SeriesPage/SeriesCreate";
 
 const App = () => {
   return (
@@ -9,7 +12,10 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/column/create" element={<ColumnCreatePage />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/seriesList" element={<SeriesList />} />
+          <Route path="/seriesList/create" element={<SeriesCreate />} />
         </Routes>
       </Router>
     </div>
