62abe1787c11c0bb723405915fbbf851a399d3e5
Pua-Sangjun <daniellyu99@ailo.kr>
2023-08-15 23:45:29 +0900

    prettier changes

--- hackathon/src/components/CategoryModal/CategoryModal.js
+++ hackathon/src/components/CategoryModal/CategoryModal.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import styles from "./CategoryModal.module.css"
-import images from '../../assets/images/images';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import styles from "./CategoryModal.module.css";
+import images from "../../assets/images/images";
 
 const tagList = [
   "주식",
@@ -14,80 +15,110 @@ const tagList = [
   "대출",
   "환전",
   "재태크",
-]
+];
 
-// props로 모달창 관리 state(boolean)의 setter를 넘겨준다. : 
+// props로 모달창 관리 state(boolean)의 setter를 넘겨준다. :
 // 예시 : <CategoryModal setModalActive={모달창 관리 state의 setter}/>
 
-const CategoryModal = ({setModalActive})=>{
-  const [addedCategory, setAddedCategory] = useState([])
-  const [addedCategoryJSX, setAddedCategoryJSX] = useState([])
-  const [notAddedCategoryJSX, setNotaddedCategoryJSX] = useState([])
-  const [input, setInput] = useState("")
+const CategoryModal = ({ setModalActive }) => {
+  const [addedCategory, setAddedCategory] = useState([]);
+  const [addedCategoryJSX, setAddedCategoryJSX] = useState([]);
+  const [notAddedCategoryJSX, setNotaddedCategoryJSX] = useState([]);
+  const [input, setInput] = useState("");
 
-  const categoryBoxHandler =  async (e) => {
-    let newAddedCategory = []
-    await(()=>{
+  const categoryBoxHandler = async (e) => {
+    let newAddedCategory = [];
+    await (() => {
       const content = e.target.innerText;
-      if(addedCategory.includes(content)){
-        newAddedCategory = addedCategory.filter((data)=>{
-          return data!==content
-        })
-        setAddedCategory(()=>newAddedCategory)
-      }else{
-        setAddedCategory((prev)=>[
-          ...prev, content
-        ])
-        newAddedCategory = addedCategory
-        newAddedCategory.push(content)
+      if (addedCategory.includes(content)) {
+        newAddedCategory = addedCategory.filter((data) => {
+          return data !== content;
+        });
+        setAddedCategory(() => newAddedCategory);
+      } else {
+        setAddedCategory((prev) => [...prev, content]);
+        newAddedCategory = addedCategory;
+        newAddedCategory.push(content);
       }
-    })()
-  }
+    })();
+  };
 
-  const inputBoxHandler = (e)=>{
+  const inputBoxHandler = (e) => {
     const inputBoxContent = e.target.value;
-    console.log(inputBoxContent)
-    setInput(()=>inputBoxContent)
-  }
+    console.log(inputBoxContent);
+    setInput(() => inputBoxContent);
+  };
 
   const modalActiveHandler = (e) => {
-    setModalActive((prev) => !prev)
-  }
+    setModalActive((prev) => !prev);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     const newAddedCategoryJSX = [];
     const newNotAddedCategoryJSX = [];
-    for(let i=0; i<tagList.length; i++){
-      const tagName = tagList[i]
-      if(addedCategory.includes(tagName)){
-        newAddedCategoryJSX.push(<div className={styles.categoryAdded} onClick={categoryBoxHandler} key={i}>{tagName}</div>)
-      }else if(tagName.includes(input)){
-        newNotAddedCategoryJSX.push(<div className={styles.categoryNotAdded} onClick={categoryBoxHandler} key={i}>{tagName}</div>)
+    for (let i = 0; i < tagList.length; i++) {
+      const tagName = tagList[i];
+      if (addedCategory.includes(tagName)) {
+        newAddedCategoryJSX.push(
+          <div
+            className={styles.categoryAdded}
+            onClick={categoryBoxHandler}
+            key={i}
+          >
+            {tagName}
+          </div>
+        );
+      } else if (tagName.includes(input)) {
+        newNotAddedCategoryJSX.push(
+          <div
+            className={styles.categoryNotAdded}
+            onClick={categoryBoxHandler}
+            key={i}
+          >
+            {tagName}
+          </div>
+        );
       }
     }
-    setAddedCategoryJSX(()=>newAddedCategoryJSX)
-    setNotaddedCategoryJSX(()=>newNotAddedCategoryJSX)
-  }, [addedCategory, input])
+    setAddedCategoryJSX(() => newAddedCategoryJSX);
+    setNotaddedCategoryJSX(() => newNotAddedCategoryJSX);
+  }, [addedCategory, input]);
 
-  return(
+  return (
     <div className={styles.background}>
       <div className={styles.modalContainer}>
-        <img src={images.cancel} alt='cancel' className={styles.cancelImage} onClick={modalActiveHandler}/>
+        <img
+          src={images.cancel}
+          alt="cancel"
+          className={styles.cancelImage}
+          onClick={modalActiveHandler}
+        />
         <span className={styles.modalTitle}>카테고리 추가 / 삭제</span>
-        <input type='text' placeholder='# 추가할 태그 이름 입력하기' className={styles.inputBox} onChange={inputBoxHandler}/>
+        <input
+          type="text"
+          placeholder="# 추가할 태그 이름 입력하기"
+          className={styles.inputBox}
+          onChange={inputBoxHandler}
+        />
         <div className={styles.categoryEditBox}>
           <div className={styles.categoryBoxAdded}>
-            <div className={styles.categoryBoxTitle}># 추가할 태그를 클릭하세요</div>
+            <div className={styles.categoryBoxTitle}>
+              # 추가할 태그를 클릭하세요
+            </div>
             <div className={styles.categoryBoxContent}>{addedCategoryJSX}</div>
           </div>
           <div className={styles.categoryBoxNotAdded}>
-            <div className={styles.categoryBoxTitle}># 제거할 태그를 클릭하세요</div>
-            <div className={styles.categoryBoxContent}>{notAddedCategoryJSX}</div>
+            <div className={styles.categoryBoxTitle}>
+              # 제거할 태그를 클릭하세요
+            </div>
+            <div className={styles.categoryBoxContent}>
+              {notAddedCategoryJSX}
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryModal
+export default CategoryModal;
--- hackathon/src/components/LoginPage/LoginPage.js
+++ hackathon/src/components/LoginPage/LoginPage.js
@@ -1,23 +1,22 @@
-import React, {useState} from "react";
-import styles from './LoginPage.module.css';
+import React, { useState } from "react";
+import styles from "./LoginPage.module.css";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-
-  const [id, setId] = useState(''); //아이디와 비밀번호가 빈칸이 아닌지 유효성 검사
-  const [pw, setPw] = useState('');
+  const [id, setId] = useState(""); //아이디와 비밀번호가 빈칸이 아닌지 유효성 검사
+  const [pw, setPw] = useState("");
   const [idValid, setIdValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
 
   const idHandler = (event) => {
     setIdValid(false);
     setId(event.target.value);
-  }
+  };
 
   const pwHandler = (event) => {
     setPwValid(false);
     setPw(event.target.value);
-  }
+  };
 
   const loginHandler = (event) => {
     event.preventDefault();
@@ -27,46 +26,55 @@ const LoginPage = () => {
       setPwValid(true);
     }
     console.log(id, pw);
-    
-  }
+  };
 
   // navigate
   const navigate = useNavigate();
 
   const passwordForgetHandler = () => {
-    navigate('#');
+    navigate("#");
   };
 
-  return(
+  return (
     <div className={styles.login_container}>
       <div className={styles.login_logo}>
-        <h1>Economys<span>Times</span></h1>
+        <h1>
+          Economys<span>Times</span>
+        </h1>
       </div>
       <form onSubmit={loginHandler}>
         <div className={styles.login_form}>
           <div className={styles.login_input}>
-            <input onChange={idHandler} type="text" placeholder="id"/>
+            <input onChange={idHandler} type="text" placeholder="id" />
           </div>
-          <div className={styles.login_input} style={{paddingBottom:'2rem'}}>
-            <input onChange={pwHandler} type="password" placeholder="password"/>
+          <div className={styles.login_input} style={{ paddingBottom: "2rem" }}>
+            <input
+              onChange={pwHandler}
+              type="password"
+              placeholder="password"
+            />
           </div>
           <div className={styles.login_button}>
             <button type="submit">Log In</button>
           </div>
         </div>
       </form>
-      {idValid && <p className={styles.login_invalid}>아이디를 입력해주세요.</p>}
-      {pwValid && <p className={styles.login_invalid}>비밀번호를 입력해주세요.</p>}
+      {idValid && (
+        <p className={styles.login_invalid}>아이디를 입력해주세요.</p>
+      )}
+      {pwValid && (
+        <p className={styles.login_invalid}>비밀번호를 입력해주세요.</p>
+      )}
       <div className={styles.login_replace}>
         <div>Kakao</div>
         <div>Naver</div>
         <div>Google</div>
       </div>
-      <div className={styles.login_forget} onClick={passwordForgetHandler}>
-        <p>Forget Password?</p>
+      <div className={styles.login_forget}>
+        <p onClick={passwordForgetHandler}>Forget Password?</p>
       </div>
     </div>
-  )
+  );
 };
 
 export default LoginPage;
