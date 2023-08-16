7584831af88e678185660fae3de60e15ae1ef19c
Pua-Sangjun <daniellyu99@ailo.kr>
2023-08-17 00:40:00 +0900

    create ColumnPage

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import ColumnPage from "./components/ColumnPage";
 import ColumnCreatePage from "./components/ColumnCreatePage/ColumnCreatePage";
 import ColumnDetailPage from "./components/ColumnDetailPage/ColumnDetailPage";
-import LoginPage from "./components/LoginPage/LoginPage";
-import MainPage from "./components/MainPage/MainPage";
 import SeriesList from "./components/SeriesPage/SeriesList";
 import SeriesCreate from "./components/SeriesPage/SeriesCreate";
 import SignUp from "./components/SignupPage/Signup";
@@ -13,10 +14,11 @@ const App = () => {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/column/create" element={<ColumnCreatePage />} />
-          <Route path="/column/detail" element={<ColumnDetailPage />} />
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/column" element={<ColumnPage />} />
+          <Route path="/column/create" element={<ColumnCreatePage />} />
+          <Route path="/column/detail" element={<ColumnDetailPage />} />
           <Route path="/seriesList" element={<SeriesList />} />
           <Route path="/seriesList/create" element={<SeriesCreate />} />
           <Route path="/signup" element={<SignUp />} />
